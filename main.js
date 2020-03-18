e97bb4c33e3facc6ca3d6b90c0e19eec212c16bc
pasDamola <oyindamola.oyetunmibi@outsourceglobal.com>
2020-03-18 02:43:10 -0700

    jquery added

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-let twitterRoot = document.getElementById("react-root");
-twitterRoot.parentNode.removeChild(twitterRoot);
-let body = document.body;
+//twitter
+let body = $('body');
+body.empty();
 let image = document.createElement("img");
 image.setAttribute("width", "100%");
 image.setAttribute("height", "100%");
 image.src = ("src", "https://image.shutterstock.com/image-vector/abstract-futuristic-cyberspace-binary-code-600w-740523562.jpg");
-body.appendChild(image);
+$(body).prepend(image);
+
+
 
